ce5897ac8f16db6706b9d242049fc376751c4072
Arthur Fayzrakhmanov <a.fayzrakhmanov@tinkoff.ru>
2022-08-26 18:45:14 +0300

    Clean up

--- chadrc.lua
+++ chadrc.lua
@@ -35,28 +35,9 @@ M.plugins = {
   },
 }
 
-function Duck(f)
-  local r, c = unpack(vim.api.nvim_win_get_cursor(0))
-  local current_line = vim.api.nvim_get_current_line()
-  local loc, x, y, lnum, w, z, term, a, b = string.find(current_line, "(.+):(%d+): (.+)")
-  print("Found: ", term, " in ", loc, " at ", lnum, "line")
-  -- if term then
-  --   print('Found: ', term, ' in ', loc, ' at ', lnum, 'line')
-  -- else
-  --   print('o_O', current_line)
-  -- end
-  -- print("! " .. r .. ", " .. c)
-  local content = vim.api.nvim_buf_get_text(0, 0, 0, 0, 5, {})
-  for _, l in ipairs(content) do
-    print(">>" .. vim.inspect(l))
-  end
-  require("telescope.builtin").find_files { search_file = content[0] }
-end
-
 M.mappings = {
   telescope = {
     n = {
-      ["<leader>xx"] = { "<cmd> lua Duck() <CR>", "  find dynamic symbol" },
       ["<leader>fs"] = { "<cmd> :Telescope lsp_dynamic_workspace_symbols <CR>", "  find dynamic symbol" },
       ["<leader>fr"] = { "<cmd> :Telescope lsp_references <CR>", " referenses" },
       ["<leader>fd"] = { "<cmd> :Telescope diagnostics <CR>", "  diagnostics" },
